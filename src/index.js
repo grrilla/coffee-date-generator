3e54c0321a20ef4506a4c279b456007cf8fb543d
Michael Longauer <mike.longauer@gmail.com>
2022-02-03 17:33:07 -0500

    build: initial tests and functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-module.exports = (input) => `${input}-suffix`;
+function findBestPartner(dateCandidate, dateCandidates) {
+	// pair off with next participant with !dep && count=0
+	// increment count for both
+	for (let i = 0; i < dateCandidates; i++) {
+		const possibleCandidate = dateCandidates[i];
+		if (dateCandidate.department && possibleCandidate.department && dateCandidate.department !== possibleCandidate.department) {
+			// thingz
+		}
+	}
+
+	return { foo: 'bar' };
+}
+
+function pairPeople(participants) {
+	const { length } = participants;
+	const pairings = { matchPool: [] };
+
+	if (length < 1) {
+		return pairings;
+	}
+
+	const dateCandidates = participants.map((value) => {
+		return { ...value, dateCount: 0 };
+	});
+
+	// name, department, dateCount
+	// pair off with next participant with !dep && count=0
+	// increment count for both
+	// go next; if already count > 0, go next immediately
+	// else pair off as above
+	//
+
+	for (let i = 0; i < length; i++) {
+		if (dateCandidates[i].dateCount === 0) {
+			const dateCandidate = dateCandidates[i];
+			pairings.matchPool.push({
+				personOne: dateCandidate,
+				personTwo: findBestPartner(dateCandidate, dateCandidates),
+			});
+		}
+	}
+
+	return pairings;
+}
+
+module.exports = { pairPeople, findBestPartner };
